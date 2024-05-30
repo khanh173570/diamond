47a65c4b8d60e08aef9e8777541fc23f60eb1d3d
AnhMinh <minhnase173546@fpt.edu.vn>
2024-05-30 19:37:56 +0700

    them

--- ProjectAdmin/vite-project/src/App.jsx
+++ ProjectAdmin/vite-project/src/App.jsx
@@ -1,13 +1,13 @@
 
-
+import Header from './component/header/Header.jsx'
 import './App.css'
 
 function App() {
- 
+  
 
   return (
      <div>
-      day la app tao 
+     
       </div>
      
     
--- ProjectStaff/src/component/Header/Header.jsx
+++ ProjectStaff/src/component/Header/Header.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
-
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function Header() {
     return (
