a61b489f9157e90fdd1540c321c2a27b5e6eca87
F4t17 <97109621+F4t17@users.noreply.github.com>
2024-06-01 10:26:22 +0700

    Update OrderDetailDTO.java

--- src/main/java/org/swp391/valuationdiamond/dto/OrderDetailDTO.java
+++ src/main/java/org/swp391/valuationdiamond/dto/OrderDetailDTO.java
@@ -1,5 +1,19 @@
 package org.swp391.valuationdiamond.dto;
 
-public class OrderDetailDTO {
+import lombok.*;
 
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class OrderDetailDTO {
+    String order_detailId;
+    Date received_date;
+    Date expired_received_date;
+    Float unit_price;
+    String Img;
+    String status;
 }
