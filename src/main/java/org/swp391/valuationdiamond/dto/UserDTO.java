12dd8664fa6d45c321ef42184f7483c8717daa99
F4t17 <97109621+F4t17@users.noreply.github.com>
2024-06-01 10:26:42 +0700

    Update UserDTO.java

--- src/main/java/org/swp391/valuationdiamond/dto/UserDTO.java
+++ src/main/java/org/swp391/valuationdiamond/dto/UserDTO.java
@@ -1,5 +1,21 @@
 package org.swp391.valuationdiamond.dto;
 
-public class UserDTO {
+import lombok.*;
 
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserDTO {
+    String user_id;
+    String password;
+    String first_name;
+    String lastname;
+    Date birthday;
+    String phone_number;
+    String email;
+    String address;
+    String role;
 }
