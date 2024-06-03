1176fd0c3c128d13ee403ea8cba4fe9c22d3d130
longlnse <112839843+longlnse@users.noreply.github.com>
2024-06-03 20:45:54 +0700

    evaluation result controller

--- src/main/java/org/swp391/valuationdiamond/controller/EvaluationResultController.java
+++ src/main/java/org/swp391/valuationdiamond/controller/EvaluationResultController.java
@@ -1,5 +1,30 @@
 package org.swp391.valuationdiamond.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.swp391.valuationdiamond.dto.EvaluationResultDTO;
+import org.swp391.valuationdiamond.entity.EvaluationResult;
+import org.swp391.valuationdiamond.service.EvaluationResultServiceImp;
+import java.util.List;
+@RestController
+@RequestMapping("/evaluation_results")
 public class EvaluationResultController {
+    @Autowired
+    private EvaluationResultServiceImp evaluationResultServiceImp;
+    @PostMapping
+    EvaluationResult createEvaluationResult(@RequestBody EvaluationResultDTO evaluationResultDTO) {
+        return evaluationResultServiceImp.createEvaluationResult(evaluationResultDTO);
 
+    }
+    @GetMapping("/getEvaluationResults")
+    List<EvaluationResult> getEvaluationResults(){
+        {
+            return evaluationResultServiceImp.getEvaluationResult();
+        }
+    }
+    @GetMapping("/getEvaluationResults/{evaluationResultId}")
+    EvaluationResult getEvaluationResult(@PathVariable String evaluationResultId) {
+        return evaluationResultServiceImp.getEvaluationResult(evaluationResultId);
+    }
 }
+
