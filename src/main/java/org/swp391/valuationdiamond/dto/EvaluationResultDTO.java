7dc40422bc557a35a564b7d7844888f7195ef2db
longlnse <112839843+longlnse@users.noreply.github.com>
2024-06-03 20:46:05 +0700

    evaluation result DTO update

--- src/main/java/org/swp391/valuationdiamond/dto/EvaluationResultDTO.java
+++ src/main/java/org/swp391/valuationdiamond/dto/EvaluationResultDTO.java
@@ -1,5 +1,31 @@
 package org.swp391.valuationdiamond.dto;
 
+
+import lombok.*;
+
+import java.math.BigDecimal;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class EvaluationResultDTO {
+    String evaluationResultId;
+    String diamondOrigin;
+    String measurements;
+    String proportions;
+    String shapeCut;
+    BigDecimal caratWeight;
+    String color;
+    String clarity;
+    String cut;
+    String symmetry;
+    String polish;
+    String fluorescence;
+    String description;
+
+
+
+
 
 }
