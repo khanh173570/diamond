7141fcb27686f0a6ca8e10adf7fa6139ca6d7057
longlnse <112839843+longlnse@users.noreply.github.com>
2024-06-05 10:41:18 +0700

    result list update

--- src/main/java/org/swp391/valuationdiamond/repository/EvaluationResultRepository.java
+++ src/main/java/org/swp391/valuationdiamond/repository/EvaluationResultRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 @Repository
 public interface EvaluationResultRepository extends JpaRepository<EvaluationResult, String> {
 
-    List<EvaluationResult> getEvaluationResult();
     }
 
