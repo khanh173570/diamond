68c603fd633bdf08d02d47680d5070e4dbbcc529
AnhMinh <minhnase173546@fpt.edu.vn>
2024-06-12 17:41:35 +0700

    sua 1 so cai + them may cai save cho phu hop yeu cau cua trang nay

--- DiamondProject/src/page/pageAdmin/ManageSchedule/ManageScheldule.jsx
+++ DiamondProject/src/page/pageAdmin/ManageSchedule/ManageScheldule.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import '../ManageSchedule/ManageSchedule.css'
+import Swal from 'sweetalert2';
+
+import './ManageSchedule.css';
+
 export const ManageSchedule = () => {
   const [dataManage, setDataManage] = useState([]);
   const [evaluationStaffIds, setEvaluationStaffIds] = useState([]);
   const [selectedEvaluationStaff, setSelectedEvaluationStaff] = useState({});
-  const [showFormDetail, setShowFormDetail]= useState([false]);
+
+
   // Fetch orderDetail data
   useEffect(() => {
     const fetchData = async () => {
@@ -27,7 +31,7 @@ export const ManageSchedule = () => {
   useEffect(() => {
     const fetchStaffIds = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users'); // Replace with your API endpoint
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const data = await response.json();
         setEvaluationStaffIds(data);
       } catch (error) {
@@ -37,8 +41,7 @@ export const ManageSchedule = () => {
 
     fetchStaffIds();
   }, []);
-    const handleShowDetail = () => setShowFormDetail(true);
-    const handleCloseDetail =() => setShowFormDetail(false);
+ 
   const handleOnChangeValuationStaff = (orderDetailId, value) => {
     setSelectedEvaluationStaff((prevState) => ({
       ...prevState,
@@ -61,6 +64,13 @@ export const ManageSchedule = () => {
       });
       const data = await response.json();
       console.log(data);
+
+      Swal.fire({
+        title: 'Thành công!',
+        text: 'Dữ liệu đã được lưu thành công.',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
     } catch (error) {
       console.error('Error updating evaluation ID:', error);
     }
@@ -69,12 +79,14 @@ export const ManageSchedule = () => {
   return (
     <>
       <h2 className="text-center p-4 my-4">Schedule Valuation Diamond</h2>
+
       <Table striped bordered className="fs-5">
         <thead>
           <tr>
             <th>OrderId</th>
-            <th>Quantity</th>
-            <th>Receive Date</th>
+            <th>OrderDetailId</th>
+            <th>Order Date</th>
+            <th>Type Service</th>
             <th></th>
             <th></th>
           </tr>
@@ -83,8 +95,9 @@ export const ManageSchedule = () => {
           {dataManage.map((data) => (
             <tr key={data.id}>
               <td>{data.id}</td>
-              <td>{data.id}</td>
+              <td>{data.username}</td>
               <td>11/1/1111</td>
+              <td>{data.email}</td>
               <td>
                 <Form.Select
                   onChange={(e) => handleOnChangeValuationStaff(data.id, e.target.value)}
@@ -99,8 +112,9 @@ export const ManageSchedule = () => {
                 </Form.Select>
               </td>
               <td>
-                <Button onClick={() => handleSendClick(data.id)}
-                        className='btn text-light'>SEND</Button>
+                <Button onClick={() => handleSendClick(data.id)} className='btn text-light'>
+                  SEND
+                </Button>
               </td>
             </tr>
           ))}
