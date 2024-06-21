25079f6611ef754b5f8e7e79f4f01d0d9721fb71
khanhtran <khanhtpse173570>
2024-06-21 20:46:48 +0700

    delete

--- ProjectCustomer/src/component/page/Blog.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-function Blog(){
-    return <div>Hello ca nha nhiu cua kem</div>
-}
-export default Blog
--- ProjectStaff/src/page/PersonalInformation.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const PersonalInformation = () => {
-  return (
-    <div>PersonalInformation</div>
-  )
-}
