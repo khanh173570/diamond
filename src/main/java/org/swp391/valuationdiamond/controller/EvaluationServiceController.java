834de0a9f75c00fb08eef064f9e714ace533ec7c
giakoii <khoipgse172073@fpt.edu.vn>
2024-06-23 15:14:49 +0700

    CRUD cac ham service

--- src/main/java/org/swp391/valuationdiamond/controller/EvaluationServiceController.java
+++ src/main/java/org/swp391/valuationdiamond/controller/EvaluationServiceController.java
@@ -1,12 +1,9 @@
 package org.swp391.valuationdiamond.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.swp391.valuationdiamond.dto.EvaluationServiceDTO;
 import org.swp391.valuationdiamond.entity.EvaluationService;
-import org.swp391.valuationdiamond.entity.OrderDetail;
 import org.swp391.valuationdiamond.service.EvaluationServiceServiceImp;
 
 import java.util.List;
@@ -16,6 +13,21 @@ import java.util.List;
 public class EvaluationServiceController {
     @Autowired
     private EvaluationServiceServiceImp evaluationServiceServiceImp;
+
+    @PostMapping("/create")
+    EvaluationService createService(@RequestBody EvaluationServiceDTO evaluationServiceDTO) {
+        return evaluationServiceServiceImp.createService(evaluationServiceDTO);
+    }
+
+    @PutMapping("/update/{serviceId}")
+    EvaluationService updateService(@PathVariable("serviceId") String serviceId, @RequestBody EvaluationServiceDTO evaluationServiceDTO) {
+        return evaluationServiceServiceImp.updateService(serviceId, evaluationServiceDTO);
+    }
+
+    @GetMapping("/getServiceById/{serviceId}")
+    EvaluationService getServiceById(@PathVariable("serviceId") String serviceId) {
+        return evaluationServiceServiceImp.getServiceById(serviceId);
+    }
 
     @GetMapping("/getServices")
     List<EvaluationService> getServices() {
--- src/main/java/org/swp391/valuationdiamond/service/EvaluationServiceServiceImp.java
+++ src/main/java/org/swp391/valuationdiamond/service/EvaluationServiceServiceImp.java
@@ -2,9 +2,14 @@ package org.swp391.valuationdiamond.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.swp391.valuationdiamond.dto.EvaluationServiceDTO;
 import org.swp391.valuationdiamond.entity.EvaluationService;
+import org.swp391.valuationdiamond.entity.EvaluationServicePriceList;
 import org.swp391.valuationdiamond.entity.Order;
+import org.swp391.valuationdiamond.entity.OrderDetail;
+import org.swp391.valuationdiamond.repository.EvaluationServicePriceListReponsitory;
 import org.swp391.valuationdiamond.repository.EvaluationServiceRepository;
+import org.swp391.valuationdiamond.repository.OrderDetailRepository;
 
 import java.util.List;
 @Service
@@ -12,8 +17,43 @@ public class EvaluationServiceServiceImp {
     @Autowired
     EvaluationServiceRepository evaluationServiceRepository;
 
-    public List<EvaluationService> getServices() {
+    @Autowired
+    EvaluationServicePriceListReponsitory evaluationServicePriceListReponsitory;
 
+    @Autowired
+    OrderDetailRepository orderDetailRepository;
+
+    //============================================ Hàm create ========================================
+    public EvaluationService createService(EvaluationServiceDTO evaluationServiceDTO) {
+        EvaluationService evaluationService = EvaluationService.builder()
+                .serviceId(evaluationServiceDTO.getServiceId())
+                .serviceType(evaluationServiceDTO.getServiceType())
+                .serviceDescription(evaluationServiceDTO.getServiceDescription())
+                .build();
+        return evaluationServiceRepository.save(evaluationService);
+    }
+    //============================================ Hàm update ========================================
+    public EvaluationService updateService(String serviceId,EvaluationServiceDTO evaluationServiceDTO) {
+        EvaluationService evaluationService = evaluationServiceRepository.findById(serviceId)
+                .orElseThrow(() -> new RuntimeException("Service not found"));
+        if(evaluationServiceDTO.getServiceType() != null){
+            evaluationService.setServiceType(evaluationServiceDTO.getServiceType());
+        }
+        if(evaluationServiceDTO.getServiceDescription() != null){
+            evaluationService.setServiceDescription(evaluationServiceDTO.getServiceDescription());
+        }
+        return evaluationServiceRepository.save(evaluationService);
+    }
+    //=============================================== Các hàm Get ========================================
+    public List<EvaluationService> getServices() {
         return  evaluationServiceRepository.findAll();
     }
+
+    public EvaluationService getServiceById(String serviceId) {
+        EvaluationService evaluationService = evaluationServiceRepository.findById(serviceId)
+                .orElseThrow(() -> new RuntimeException("Service not found"));
+        return evaluationService;
+    }
+
+
 }
