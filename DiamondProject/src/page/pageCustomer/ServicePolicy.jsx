7793a684cc170f6ad62ba4819a59573e53266d28
khanhtran <khanhtpse173570>
2024-06-23 19:08:20 +0700

    updateServicePolicy

--- DiamondProject/src/page/pageCustomer/ServicePolicy.jsx
+++ DiamondProject/src/page/pageCustomer/ServicePolicy.jsx
@@ -1,116 +1,162 @@
-import React from 'react';
-import {Col, Image,Row} from "react-bootstrap";
+import React from "react";
+import { Col, Image, Row } from "react-bootstrap";
 
 const ServicePolicy = () => {
   return (
-    <div >  
-        <Row>  
-    <Col md="8" style={{marginBottom:"50px"}}>
-      <h1 className='' style={{marginTop:"50px", marginLeft:"600px"}}>Service Policy</h1>
-        <div className='' style={{marginLeft:"200px"}} >  
-      <section>
-        <div style={{ fontWeight: 'bold' }}>1. Diamond Valuation Process</div>
-        <p >
-          This section may detail the steps involved in the diamond valuation process, including how diamonds are evaluated, graded, and priced.
-        </p>
-      </section>
-
-      <section >
-        <div style={{ fontWeight: 'bold' }}>2. Service Offerings</div>
-        <p>
-          It describes the range of services provided by the shop, such as diamond appraisal, certification, grading, and valuation for various purposes like insurance or resale.
-        </p>
-      </section>
-
-      <section>
-        <div style={{ fontWeight: 'bold' }}>3. Turnaround Time</div>
-        <p>
-          This specifies the estimated time it takes for the shop to complete the valuation process and provide the customer with the results.
-        </p>
-      </section>
-
-      <section>
-        <div style={{ fontWeight: 'bold' }}>4. Fees and Charges</div>
-        <p>
-          It outlines the fees associated with each service offered, including any additional charges for expedited services or special requests.
-        </p>
-      </section>
-
-      <section>
-        <div style={{ fontWeight: 'bold' }}>5. Terms of Payment</div>
-        <p>
-          This section explains the accepted methods of payment, billing procedures, and any applicable taxes or surcharges.
-        </p>
-      </section>
-
-      <section>
-        <div style={{ fontWeight: 'bold' }}>6. Confidentiality</div>
-        <p>
-          The policy may include clauses about the confidentiality of the customer information and the privacy measures in place to protect their data.
-        </p>
-      </section>
-
-      <section>
-        <div style={{ fontWeight: 'bold' }}>7. Quality Assurance</div>
-        <p>
-          It may include information about the shop commitment to accuracy, reliability, and adherence to industry standards in diamond valuation.
-        </p>
-      </section>
-
-      <section>
-        <div style={{ fontWeight: 'bold' }}>8. Disclaimers</div>
-        <p>
-          This part typically includes disclaimers regarding the limitations of the valuation process, such as the subjective nature of diamond grading and the inherent variability in diamond prices.
-        </p>
-      </section>
-
-      <section>
-        <div style={{ fontWeight: 'bold' }}>9. Customer Responsibilities</div>
-        <p>
-          It outlines the responsibilities of the customer, such as providing accurate information about the diamond and complying with the shop guidelines and procedures.
-        </p>
-      </section>
-
-      <section>
-        <div style={{ fontWeight: 'bold' }}>10. Refund and Return Policy</div>
-        <p>
-          This section explains the shop policy regarding refunds, returns, and exchanges for services rendered, including any conditions or limitations.
-        </p>
-      </section>
-
-      <section>
-        <div style={{ fontWeight: 'bold' }}>11. Dispute Resolution</div>
-        <p>
-          It may include information about how disputes or disagreements will be handled, such as through arbitration or mediation.
-        </p>
-      </section>
-
-      <section>
-        <div style={{ fontWeight: 'bold' }}>12. Changes to the Policy</div>
-        <p>
-          The policy might state the shop right to modify or update the service policy and how customers will be notified of any changes.
-        </p>
-        
-      </section>
-      </div>
-    </Col>
-    <Col md={4}>
-    <h2 className='' style={{marginTop: "200px", marginLeft:"50px"}}>Clear Policy - Loyal Customers</h2>
-    <Image
-      src="/src/assets/assetsCustomer/servicepolicy.png"
-      fluid
-      style={{ borderRadius: "4px", marginTop: "10ppx", width: "50%", marginLeft:"130px" }}
-    />
-    <h2 className='' style={{marginTop: "100px", marginLeft:"50px"}}>Diamond Valuation- Life Valuation</h2> 
-    <Image
-      src="/src/assets/assetsCustomer/diamond.png"
-      fluid
-      style={{ borderRadius: "4px", marginTop: "10px", width: "50%", marginLeft:"130px" }}
-    /> 
- 
-  </Col>
-  </Row>
-  </div>
+    <div>
+      <Row>
+        <Col md="8" style={{ marginBottom: "50px" }}>
+          <h1 className="" style={{ marginTop: "50px", marginLeft: "600px", marginBottom:"50px" }}>
+            Service Policy
+          </h1>
+          <p style={{ marginLeft: "50px" }}>
+            <h2 className="text-center"> 1. QUI ĐỊNH CHUNG</h2>
+            <div>
+              1.1. Khách hàng phải cung cấp đầy đủ và chính xác các thông tin ở
+              PHIẾU YÊU CẦU GIÁM ĐỊNH. DIAMOND VALUATION sẽ căn cứ vào PHIẾU YÊU
+              CẦU GIÁM ĐỊNH tiến hành tạo BIÊN NHẬN GIÁM ĐỊNH. Các thông tin:
+              Tên Khách hàng, địa chỉ, số điện thoại, Email (nếu có), yêu cầu
+              dịch vụ. Trường hợp Niêm phong lại hay Cấp lại Giấy Giám định,
+              Khách hàng cần cung cấp số số điện thoại của Mẫu Giám định (có thể
+              kèm theo Niêm phong hoặc Giấy Giám định cũ).
+            </div>
+            <div>
+              1.2. DIAMOND VALLUATION không chịu trách nhiệm pháp lý về tính xác
+              thực các thông tin do Khách hàng cung cấp.
+            </div>
+            <div>
+              1.3. BIÊN NHẬN GIÁM ĐỊNH được xem như HỢP ĐỒNG DỊCH VỤ GIÁM ĐỊNH
+              giữa DIAMOND VALUATIONvà Khách hàng. BNGĐ được in thành hai bản,
+              một bản do DIAMONDgiữ và một bản do Khách hàng giữ.
+            </div>
+            <div>
+              1.4. Trường hợp Khách hàng mong muốn sự thay đổi HĐDVGĐ phải gọi
+              điện thông báo cho DIAMOND VALUATION trong vòng 6 giờ kể từ khi
+              gửi Mẫu (không áp dụng cho yêu cầu Giám định nhanh). Sau thời gian
+              này, Khách hàng sẽ không được thay đổi bất kỳ nội dung nào trên
+              HĐDVGĐ.
+            </div>
+            <div>
+              1.5. BNGĐ chỉ xác nhận đúng SỐ LƯỢNG Mẫu Giám định. Các thông tin
+              về trọng lượng, kích thước, màu sắc, dạng cắt mài..., tên Mẫu do
+              Khách hàng cung cấp, …chỉ mang tính chất tham khảo và nhận diện.
+            </div>
+            <div>
+              1.6. DIAMOND VALUATION không xác định bản chất thực tế Mẫu Giám
+              định là loại đá gì, thiên nhiên, tổng hợp, có xử lý hay không tại
+              thời điểm nhận Mẫu Giám định.
+            </div>
+            <div>
+              1.7. BNGĐ chỉ có giá trị trong vòng 30 ngày kể từ ngày gửi Mẫu
+              Giám định. Sau 30 ngày nếu Khách hàng vẫn chưa đến nhận kết quả .
+              Nếu trong khoảng thời gian này Khách hàng vẫn không đến lấy hoặc
+              khi BNGĐ đã hết giá trị mà DIAMOND VALUATION không liên hệ được với Khách hàng
+              thì DIAMOND VALUATION sẽ tiến hành lập biên bản niêm phong các Mẫu
+              Giám định và có hướng giải quyết theo Quy định của Công ty.
+            </div>
+            <div>
+              1.8. Trong trường hợp việc Giám định có trở ngại hay cần thay đổi,
+              DIAMOND VALUATION sẽ gọi điện thông báo, bàn bạc với Khách hàng trên tinh thần
+              hợp tác, tôn trọng lẫn nhau để đưa ra hướng giải quyết tốt nhất.
+            </div>
+            <div>
+              1.9. DIAMOND VALUATION thực hiện việc Giám định một cách độc lập và có quyền từ
+              chối yêu cầu Giám định khi Khách hàng không cung cấp đầy đủ thông
+              tin trên PHIẾU YÊU CẦU GIÁM ĐỊNH hoặc xét thấy không đảm bảo về
+              các yếu tố kỹ thuật, chuyên môn.
+            </div>
+            <div>
+              1.10. Giấy Giám định của DIAMOND VALUATION không phải là giấy Bảo
+              hành, không định giá Mẫu Giám định đính kèm.
+            </div>
+            <div>
+              1.11. DIAMOND VALUATION không giữ vai trò đại diện hay đảm bảo cho Giấy Giám
+              định vì Giấy giám định chỉ mô tả trung thực những đặc tính và các
+              thông số kỹ thuật của Mẫu Giám định sau khi đã được Giám định theo
+              đúng tiêu chuẩn và thiết bị hiện có của DIAMOND VALUATIONtại thời
+              điểm Giám định. Kết quả Giám định trong những lần Giám định sau có
+              thể thay đổi tùy thuộc vào: · Thời điểm, cách thức và Chuyên gia
+              thực hiện Giám định. · Những thay đổi và cải tiến công nghệ, thiết
+              bị dùng Giám định giúp các Chuyên gia phát hiện những yếu tố khác
+              của Mẫu mà trước đó DIAMOND VALUATION không phát hiện được.
+            </div>
+            <div>
+              1.12. DIAMOND VALUATION sẽ không chịu trách nhiệm về pháp lý cho bất kì sự
+              tranh chấp, hư hại, kể cả những thiệt hại của Khách hàng xuất phát
+              từ lỗi sai sót trên Giấy Giám định và trong việc lưu hành Giấy
+              Giám định.
+            </div>
+            <div style={{marginBottom:"50px"}}>
+              1.13. Quy định Niêm phong lại (Seal lại) · DIAMOND VALUATION sẽ tiến hành Niêm
+              phong lại các Mẫu Giám định khi đã xác định đúng các thông số kĩ
+              thuật. Trường hợp các thông số kĩ thuật có sự sai lệch hoặc phát
+              sinh các lỗi trong quá trình sử dụng Mẫu từ phía Khách hàng (rạn,
+              nứt, mẻ cạnh, mẻ đáy, cắt, mài, đánh bóng,…) sẽ KHÔNG Niêm phong
+              lại và thông báo trả lại Mẫu Giám định cho Khách hàng. · Trường
+              hợp còn niêm phong cũ do DIAMOND VALUATION cung cấp mà tình trạng
+              Mẫu Giám định vẫn còn nguyên vẹn thì DIAMOND VALUATION sẽ mở niêm phong cũ và
+              niêm phong mới lại theo yêu cầu của Khách hàng. · Trường hợp còn
+              niêm phong cũ do DIAMOND VALUATION cung cấp nhưng tình trạng Mẫu
+              Giám định không còn nguyên vẹn (có dấu hiệu rạn, nứt, …) mà Khách
+              hàng vẫn có nhu cầu và đồng ý Niêm phong lại thì các Mẫu này phải
+              được kiểm tra tình trạng Mẫu và lập biên bản xác nhận tình trạng
+              Mẫu trước sự chứng kiến của đại diện DIAMOND VALUATION và Khách
+              hàng. Niêm phong cũ này phải do chính Khách hàng mở. Mọi rủi ro,
+              khiếu nại về sau đối với trường hợp này DIAMOND VALUATION hoàn toàn không chịu
+              trách nhiệm.
+            </div>
+            <h2  className="text-center">2. CAM KẾT CỦA KHÁCH HÀNG</h2>
+            <div style={{marginBottom:"50px"}}>
+            Khách hàng có trách nhiệm kiểm tra đúng số lượng Mẫu Giám định, chất
+            lượng niêm phong, số lượng và chất lượng Giấy Giám định (nếu có)
+            trước khi rời khỏi quầy giao dịch. Mọi khiếu nại của Khách hàng liên
+            quan đến các vấn đề nêu trên khi rời khỏi quầy giao dịch sẽ không
+            được giải quyết.
+            </div>
+            <h2 className="text-center">3. CAM KẾT CỦA DIAMOND VALUATION</h2>
+            <div>
+              DIAMOND VALUATION cam kết thực hiện đúng nội dung Giám định đã
+              thỏa thuận với Khách hàng dựa trên kiến thức, năng lực kỹ thuật,
+              chuyên môn hiện có để cung cấp tốt nhất dịch vụ Giám định cho
+              Khách hàng với đầy đủ trách nhiệm của mình theo đúng các quy định
+              hiện hành. DIAMOND VALUATION cam kết bảo mật toàn bộ thông tin cá
+              nhân và kết quả Giám định của Khách hàng, không cung cấp thông tin
+              về việc Giám định cho bên thứ ba nào biết nếu không có sự đồng ý
+              bằng văn bản của Khách hàng, ngoại trừ các quy định khác của pháp
+              luật.
+            </div>
+          </p>
+        </Col>
+        <Col md={4}>
+          <h2 className="" style={{ marginTop: "200px", marginLeft: "50px" }}>
+            Clear Policy - Loyal Customers
+          </h2>
+          <Image
+            src="/src/assets/assetsCustomer/servicepolicy.png"
+            fluid
+            style={{
+              borderRadius: "4px",
+              marginTop: "10ppx",
+              width: "50%",
+              marginLeft: "130px",
+            }}
+          />
+          <h2 className="" style={{ marginTop: "100px", marginLeft: "50px" }}>
+            Diamond Valuation- Life Valuation
+          </h2>
+          <Image
+            src="/src/assets/assetsCustomer/diamond.png"
+            fluid
+            style={{
+              borderRadius: "4px",
+              marginTop: "10px",
+              width: "50%",
+              marginLeft: "130px",
+            }}
+          />
+        </Col>
+      </Row>
+    </div>
   );
 };
 
