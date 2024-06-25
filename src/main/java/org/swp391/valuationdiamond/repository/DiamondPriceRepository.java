9b52a3a720f30ba7c5c6cec82c674a95894f54d4
F4t17 <phattvse172155@fpt.edu.vn>
2024-06-25 17:43:16 +0700

    add: search diamond similar

--- src/main/java/org/swp391/valuationdiamond/repository/DiamondPriceRepository.java
+++ src/main/java/org/swp391/valuationdiamond/repository/DiamondPriceRepository.java
@@ -4,7 +4,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.swp391.valuationdiamond.entity.DiamondPrice;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface DiamondPriceRepository extends JpaRepository<DiamondPrice, String> {
+    List<DiamondPrice> findByDiamondOriginAndShapeAndCaratWeightBetweenAndColorAndClarityAndCutAndFluorescenceAndPolishAndSymmetryAndPriceBetween(
+            String diamondOrigin, String shape, BigDecimal caratWeightMin, BigDecimal caratWeightMax,
+            String color, String clarity, String cut, String fluorescence, String polish, String symmetry,
+            BigDecimal priceMin, BigDecimal priceMax
+    );
+    List<DiamondPrice> findByDiamondOriginLikeAndShapeLikeAndCaratWeightBetweenAndColorLikeAndClarityLikeAndCutLikeAndFluorescenceLikeAndPolishLikeAndSymmetryLikeAndPriceBetween(
+            String diamondOrigin, String shape, BigDecimal caratWeightMin, BigDecimal caratWeightMax,
+            String color, String clarity, String cut, String fluorescence, String polish, String symmetry,
+            BigDecimal priceMin, BigDecimal priceMax);
+    List<DiamondPrice> findDiamondsByCaratWeightOrPriceRange(BigDecimal caratMin, BigDecimal caratMax, BigDecimal priceMin, BigDecimal priceMax);
 
 }
