a48e85ef4f09564b68b3a0b6dad68297a87c0ba0
longlnse <112839843+longlnse@users.noreply.github.com>
2024-06-30 15:42:24 +0800

    create date change

--- src/main/java/org/swp391/valuationdiamond/service/EvaluationResultServiceImp.java
+++ src/main/java/org/swp391/valuationdiamond/service/EvaluationResultServiceImp.java
@@ -128,10 +128,9 @@ public class EvaluationResultServiceImp {
         if (evaluationResultDTO.getImg() != null) {
             result.setImg(evaluationResultDTO.getImg());
         }
-        if (evaluationResultDTO.getDate() != null) {
-            result.setDate(evaluationResultDTO.getDate());
+        if (evaluationResultDTO.getCreateDate() != null) {
+            result.setCreateDate(evaluationResultDTO.getCreateDate());
         }
-
 
         return evaluationResultRepository.save(result);
     }
